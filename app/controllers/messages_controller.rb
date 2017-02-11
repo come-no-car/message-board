00abbbcb20db9e01901d9a0f41e068826506bd69
Takuma Yonezu <yoneztyty@gmail.com>
2017-02-11 11:43:02 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,19 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    # Messageを全て取得する。
+    @messages = Message.all
   end
+  
+  def create
+    @message = Meassage.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+    
+  private
+    def message_params
+      parames.require(:message).permit(:name, :body)
+    end
+    ##ここまで
 end
