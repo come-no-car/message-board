c401d70baba05de7f7a5e2c9592937e14d310cd8
Takuma Yonezu <yoneztyty@gmail.com>
2017-02-13 08:10:42 +0000

    challenge1_2nd

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,14 +6,14 @@ class MessagesController < ApplicationController
   end
   
   def create
-    @message = Meassage.new(message_params)
+    @message = Message.new(message_params)
     @message.save
     redirect_to root_path , notice: 'メッセージを保存しました'
   end
     
   private
     def message_params
-      parames.require(:message).permit(:name, :body)
+      params.require(:message).permit(:name, :body)
     end
     ##ここまで
 end
