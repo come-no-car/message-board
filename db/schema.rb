2225fee5fd518acbf7a28452cd4fad611df9b374
Takuma Yonezu <yoneztyty@gmail.com>
2017-02-16 09:08:07 +0000

    age

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211101801) do
+ActiveRecord::Schema.define(version: 20170213143337) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
